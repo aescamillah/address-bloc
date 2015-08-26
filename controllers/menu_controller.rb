da19fe2dc3b63b621f43681c8fcbcc59044c9971
AEH <escamilla.a@gmail.com>
2015-08-26 14:30:47 -0500

    assignment 25 foundation completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -14,7 +14,8 @@ class MenuController
     puts "2 - Create an entry"
     puts "3 - Search for an entry"
     puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "5 - Delete all entries"
+    puts "6 - Exit"
     print "Enter your selection: "
     selection = gets.to_i
 
@@ -36,6 +37,10 @@ class MenuController
         read_csv
         main_menu
       when 5
+        system "clear"
+        nuke
+        main_menu
+      when 6
         puts "Good-bye!"
         exit(0)
     else
@@ -168,6 +173,24 @@ end
       puts "#{selection} is not a valid input"
       puts entry.to_s
       search_submenu(entry)
+    end
+  end
+
+  def nuke
+    print "Are you sure you want to delete all entries? (y/n): "
+    selection = gets.chomp
+    if selection == 'n'
+      system "clear"
+      main_menu
+    elsif selection == 'y'
+      @address_book.entries = []
+      system "clear"
+      puts "All entries have been deleted"
+      main_menu
+    else
+      system "clear"
+      puts "Please enter a valid input"
+      nuke
     end
   end
 
